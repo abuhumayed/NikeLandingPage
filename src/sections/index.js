e122ab076e6ce12c8fab010eeae43aa4f2d1829f
abuhumayed <ahmedthanjim@hotmail.co.uk>
2023-10-03 14:05:43 +0100

    hero

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import Footer from './Footer'
+import Suscribe from './Suscribe'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+
+export
+{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    Footer,
+    Suscribe,
+    SpecialOffer,
+    CustomerReviews
+}
