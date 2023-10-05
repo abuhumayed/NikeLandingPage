7d7e98ebe811b3566b998832b6f649517810585f
abuhumayed <ahmedthanjim@hotmail.co.uk>
2023-10-05 16:17:59 +0100

    super quality finished

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { star } from '../assets/icons'
 
 const PopularProductCard = ({imgURL,name,price}) => {
@@ -9,7 +8,7 @@ const PopularProductCard = ({imgURL,name,price}) => {
               <p className='font-montserrat text-xl text-slate-gray'>(4.5)</p>
               </div>
               <h3 className='mt-2 text-2xl font-semibold font-palanquin'>{name}</h3>
-              <p>{ price}</p>
+              <p className='mt-2 font-semibold font-montserrat text-coral-red text-21x leading-normal '>{ price}</p>
 
       </div>
   )
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,31 @@
+import { shoe8 } from "../assets/images"
+import Button from "../components/button"
+
+
+Button
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container " >
+      <div className="flex flex-col flex-1">
+        <h2 className="font-palanquin capitalize text-4xl font-bold lg:max-w-lg">
+          We Provide You  
+          <span className="text-coral-red " > Super </span> 
+          <span className="text-coral-red " > Quality </span> Shoes.
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text " >Ensuring premium comfort and style , our meticulously crafter footwear are designed to enhance your experience, providing you with unmatched quality, innovation and a touch of elegance.</p>
+        <p className="mt-6 lg:max-w-lg info-text ">Our dedication to detail and excellence ensures your satisfaction.</p>
+        <div className="mt-11 " >
+        <Button label="View Details" />
+
+        </div>
+        
+      </div>
+      <div className="flex-1 flex justify-center items-center ">
+        <img src={shoe8} alt="Nike Shoe" width={570} height ={522} className="object-contain"/>
+        </div>
+
+    </section>
   )
 }
 
