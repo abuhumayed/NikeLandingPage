78935ed339a6c37b5b63daf19c27024ecb0defe6
abuhumayed <ahmedthanjim@hotmail.co.uk>
2023-10-06 16:26:07 +0100

    Special Offer

---
+++ src/components/Button2.jsx
@@ -0,0 +1,16 @@
+const Button2 = ({label,iconURL}) =>
+{
+    return (
+        <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-white rounded-full text-slate-gray border-slate-gray">
+            {label}
+        
+           
+        {iconURL && <img
+        
+          src={iconURL} alt="arrow right icon " className="ml-2 rounded-full w-5 h-5 
+            "/>}
+      </button>
+    )
+}
+
+export default Button2
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,30 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button2 from "../components/Button2"
+import Button from "../components/button"
+Button
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img className="object-contain w-full" src={offer} alt="offer" width={773} height={687}/>
+      </div>
+      <div className="flex flex-col flex-1">
+        <h2 className="font-palanquin capitalize text-4xl font-bold lg:max-w-lg"> 
+          <span className="text-coral-red " > Special </span> 
+           Offer 
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text " >Exciting news awaits you! We're thrilled to announce a special treat for our loyal customers – a fantastic 30% discount on our entire range of Nike merchandise. This exclusive offer is our way of expressing gratitude for your unwavering support and dedication to the Nike brand.</p>
+        <p className="mt-6 lg:max-w-lg info-text ">From performance-driven sportswear to iconic sneakers that make a statement, each item in our collection reflects the spirit of innovation and excellence that defines Nike. Don't miss out on this opportunity to elevate your style and performance with top-notch products at an irresistible price. </p>
+        <div className="mt-11 flex flex-wrap gap-4" >
+        <Button label="Shop Now" iconURL={arrowRight} />
+        <Button2 label="Learn More" />
+
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
